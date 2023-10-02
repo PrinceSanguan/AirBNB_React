8e7365a754fec3b726cff62c8305b5660f115af4
Prince Sanguan <princesanguan44@gmail.com>
2023-10-02 12:54:33 +0800

    This is for React tapos na PRaktis lang everyday!

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header--container">
+      <ul>
+        <li className="header--logo">Logo</li>
+        <li className="header--logout">Logout</li>
+      </ul>
+    </header>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar--container">
+      <ul>
+        <li>About</li>
+        <li>Products</li>
+        <li>Services</li>
+        <li>Contact Us</li>
+      </ul>
+    </nav>
+  );
+}
